6cd2cd9363315522f063f2f681b0070ab9cb3df0
Keenan Bashir <besirov.k@gmail.com>
2024-07-27 13:26:48 +0400

    Items added

--- app/src/main/java/com/example/compose_curiosity_lab/draganddrop/DraggableLazyRow.kt
+++ app/src/main/java/com/example/compose_curiosity_lab/draganddrop/DraggableLazyRow.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.compose_curiosity_lab.R
 
 @Composable
 fun <T: DraggableItem>DraggableLazyRow(
@@ -37,5 +38,45 @@ data class PersonItem(
 @Preview (showBackground = true)
 @Composable
 private fun DraggableLazyRowPreview() {
-    DraggableLazyRow()
+    DraggableLazyRow(
+        items = personItems
+    )
 }
+
+val personItems = listOf(
+    PersonItem(
+        nameAndSurname = "Jennifer Logan",
+        photo = R.drawable.photo1,
+        price = 60
+    ),
+    PersonItem(
+        nameAndSurname = "Adam Barth",
+        photo = R.drawable.photo2,
+        price = 43
+    ),
+    PersonItem(
+        nameAndSurname = "Sarah Hracek",
+        photo = R.drawable.photo3,
+        price = 18
+    ),
+    PersonItem(
+        nameAndSurname = "Ian Hickson",
+        photo = R.drawable.photo4,
+        price = 74
+    ),
+    PersonItem(
+        nameAndSurname = "Eric Seidel",
+        photo = R.drawable.photo5,
+        price = 74
+    ),
+    PersonItem(
+        nameAndSurname = "Nolan Hudson",
+        photo = R.drawable.photo6,
+        price = 74
+    ),
+    PersonItem(
+        nameAndSurname = "Noah Walker",
+        photo = R.drawable.photo7,
+        price = 74
+    ),
+)
