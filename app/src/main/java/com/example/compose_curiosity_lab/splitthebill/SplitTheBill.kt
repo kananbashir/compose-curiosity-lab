8d8d2035f63b5b757df5b1707b8c647415b93974
Keenan Bashir <besirov.k@gmail.com>
2024-08-09 19:59:19 +0400

    Header text added

--- app/src/main/java/com/example/compose_curiosity_lab/splitthebill/SplitTheBill.kt
+++ app/src/main/java/com/example/compose_curiosity_lab/splitthebill/SplitTheBill.kt
@@ -1,8 +1,18 @@
 package com.example.compose_curiosity_lab.splitthebill
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 /**
  * Created on 8/9/2024
@@ -11,7 +21,20 @@ import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun SplitTheBill(modifier: Modifier = Modifier) {
-    
+    Column(
+        modifier = modifier.fillMaxSize()
+    ) {
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 20.dp),
+            text = "Fair Share",
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold,
+            fontSize = 18.sp,
+            fontFamily = FontFamily.SansSerif
+        )
+    }
 }
 
 
