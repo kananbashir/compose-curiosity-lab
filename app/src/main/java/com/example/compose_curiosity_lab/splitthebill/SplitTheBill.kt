e1eb55cd5396991920b1282da6d8d8919fbc1a07
Keenan Bashir <besirov.k@gmail.com>
2024-08-09 19:49:37 +0400

    'Split the bill' project started

---
+++ app/src/main/java/com/example/compose_curiosity_lab/splitthebill/SplitTheBill.kt
@@ -0,0 +1,25 @@
+package com.example.compose_curiosity_lab.splitthebill
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+/**
+ * Created on 8/9/2024
+ * @author Kanan Bashir
+ */
+
+@Composable
+fun SplitTheBill(modifier: Modifier = Modifier) {
+    
+}
+
+
+
+
+//-------------------------------- Preview related things --------------------------------
+@Preview (showBackground = true)
+@Composable
+private fun SplitTheBillPreview() {
+    SplitTheBill()
+}
